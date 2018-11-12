d3e4a501114e37afcb2f206ab68f60772ca3a686
Eric <eric@sharebert.com>
2018-11-12 16:45:00 -0500

    Red Icon moving

--- NYCFERRY/App.js
+++ NYCFERRY/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { StyleSheet, Text, View,Image,Dimensions } from 'react-native';
-import {Constants} from 'expo';
+import { StyleSheet, Text, View, Image, Dimensions } from 'react-native';
+import { Constants } from 'expo';
+import * as Animatable from 'react-native-animatable';
 export default class App extends React.Component {
   render() {
     console.log(Dimensions.get('window').width / 2);
@@ -13,8 +14,17 @@ export default class App extends React.Component {
             source={require('./assets/header.png')}
           />
         </View>
+        <Animatable.View
+          animation="slideInLeft" easing="linear" iterationCount="infinite" duration={2000}
+        >
+          <Image
+            resizeMode="contain"
+            style={styles.middle}
+            source={require('./assets/icon_main_red.png')}
+          />
+        </Animatable.View>
         <View
-        style={styles.footerView}>
+          style={styles.footerView}>
           <Image
             resizeMode="contain"
             style={styles.footer}
@@ -33,19 +43,26 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  header:{
+  header: {
+    position: 'absolute',
+    width: '100%',
+    marginLeft: (Dimensions.get('window').width / 2) * -1,
+    marginTop: ((Dimensions.get('window').height / 1.97) * -1) + Constants.statusBarHeight,
+    height: 200,
+  },
+  middle:{
     position: 'absolute',
     width: '100%',
     marginLeft: (Dimensions.get('window').width / 2)*-1,
-    marginTop: ((Dimensions.get('window').height / 1.97)*-1)+Constants.statusBarHeight,
+    marginBottom: 1,
     height: 200,
   },
-  footerView:{
+  footerView: {
     position: 'absolute',
     width: '100%',
     bottom: 0,
   },
-  footer:{
+  footer: {
     width: '100%',
     height: 115,
   }
